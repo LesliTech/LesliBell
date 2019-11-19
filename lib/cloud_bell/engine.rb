2c5331a9339638a847a79d8467834f334794b786
Luis Donis <ldonis.emc2@gmail.com>
2019-11-19 17:14:44 -0600

    add dynamic migration paths

--- lib/cloud_bell/engine.rb
+++ lib/cloud_bell/engine.rb
@@ -1,5 +1,15 @@
 module CloudBell
-  class Engine < ::Rails::Engine
-    isolate_namespace CloudBell
-  end
+    class Engine < ::Rails::Engine
+        isolate_namespace CloudBell
+
+        # register engine migrations path
+        initializer :append_migrations do |app|
+            unless app.root.to_s.match root.to_s
+                config.paths["db/migrate"].expanded.each do |expanded_path|
+                    app.config.paths["db/migrate"] << expanded_path
+                end
+            end
+        end
+
+    end
 end
