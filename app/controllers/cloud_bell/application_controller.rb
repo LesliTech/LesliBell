fd2ad338bc20450c594043a81b6789303403a697
Luis Donis <ldonis.emc2@gmail.com>
2019-12-02 13:21:20 -0600

    add notifications

--- app/controllers/cloud_bell/application_controller.rb
+++ app/controllers/cloud_bell/application_controller.rb
@@ -7,12 +7,10 @@ module CloudBell
         def check_account
 
             if current_user.account.bell.blank?
-
                 # insert reference to the core account
                 current_user.account.bell = CloudBell::Account.new
                 current_user.account.bell.account = current_user.account
                 current_user.account.bell.save!
-
             end
 
         end
