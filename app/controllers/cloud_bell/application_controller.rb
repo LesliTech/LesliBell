676e08c2cb5889d255b81a3f0ad9eec52d60142c
Luis Donis <ldonis.emc2@gmail.com>
2020-01-16 11:48:20 -0600

    implement app router

--- app/controllers/cloud_bell/application_controller.rb
+++ app/controllers/cloud_bell/application_controller.rb
@@ -1,19 +1,5 @@
 module CloudBell
     class ApplicationController < ApplicationLesliController
         protect_from_forgery with: :exception
-
-        before_action :check_account
-
-        def check_account
-
-            if current_user.account.bell.blank?
-                # insert reference to the core account
-                current_user.account.bell = CloudBell::Account.new
-                current_user.account.bell.account = current_user.account
-                current_user.account.bell.save!
-            end
-
-        end
-
     end
 end
