2b0888d6eef0c88c0ab9c30533169113299dd451
Carlos Hermosilla <efrainh12@galileo.edu>
2020-12-11 09:33:28 -0600

    add trello/1807 LESLI_ROOT verification

--- lib/cloud_bell/engine.rb
+++ lib/cloud_bell/engine.rb
@@ -30,9 +30,10 @@ module CloudBell
             end
         end
 
-        # register templates path in the core
+        # register templates path in the core. LESLI_ROOT is used to calculate the relative path to the core,
+        # since Rails.root is not defined at this point. You can find it in EngineDirectory/bin/rails
         config.generators do |g|
-            g.templates.unshift Pathname.new(LESLI_ROOT).join("lib", "templates")
+            g.templates.unshift Pathname.new(LESLI_ROOT).join("lib", "templates") if defined? LESLI_ROOT
         end
     end
 end
