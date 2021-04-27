44a44c14b4fb816f38953b5710398ba258f779dd
Luis Donis <ldonis.emc2@gmail.com>
2021-04-27 10:31:50 -0600

    fix:version

--- lib/cloud_bell/version.rb
+++ lib/cloud_bell/version.rb
@@ -1,9 +1,4 @@
 module CloudBell
-<<<<<<< HEAD
   VERSION = '0.1.0'
     BUILD = "20210329.0926"
-=======
-  VERSION = '0.0.10'
-    BUILD = "20210305.0959"
->>>>>>> 540735e37d58ff0736bdd6b83e05236291a75d4a
 end
