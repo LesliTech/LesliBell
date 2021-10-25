345814b7c6ced5819bca00d7ae954b22973a0e25
FranciscoRosal <ros18676@uvg.edu.gt>
2021-10-25 10:53:38 -0600

    test: updating tests

--- spec/requests/notifications/create_spec.rb
+++ spec/requests/notifications/create_spec.rb
@@ -21,7 +21,6 @@ For more information read the license file including with this software.
 # include helpers, configuration & initializers for request tests
 require 'lesli_request_helper'
 
-
 [{
     subject: Faker::Lorem.sentence(word_count: 3)
 }, {
@@ -53,12 +52,18 @@ require 'lesli_request_helper'
     body: Faker::Lorem.paragraph(sentence_count: 2),
     category: ["info", "danger", "warning", "success"].sample,
     url: Faker::Internet.url(host: 'test.lesli.cloud'),
-    role_receiver_names: [(Role.all.sample).name]
+    role_receiver_names: [(Role.all.sample).name],
+    media: Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg"),
+    payload: {},
+    notification_type: ["message", "project-alert", "event-reminder", "overdue-task"].sample,
 }, {
     subject: Faker::Lorem.sentence(word_count: 3),
     body: Faker::Lorem.paragraph(sentence_count: 2),
     category: ["info", "danger", "warning", "success"].sample,
-    url: Faker::Internet.url(host: 'test.lesli.cloud')
+    url: Faker::Internet.url(host: 'test.lesli.cloud'),
+    media: Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg"),
+    payload: {},
+    notification_type: ["message", "project-alert", "event-reminder", "due-task"].sample,
 }].each do |notification|
 
     RSpec.describe 'POST:/bell/notifications', type: :request do
--- spec/requests/notifications/show_spec.rb
+++ spec/requests/notifications/show_spec.rb
@@ -47,18 +47,24 @@ require 'lesli_request_helper'
     body: Faker::Lorem.paragraph(sentence_count: 2),
     category: ["info", "danger", "warning", "success"].sample,
     url: Faker::Internet.url(host: 'test.lesli.cloud'),
-    user_receiver_email: (User.all.sample).email
+    user_receiver_emails: [(User.all.sample).email]
 }, {
     subject: Faker::Lorem.sentence(word_count: 3),
     body: Faker::Lorem.paragraph(sentence_count: 2),
     category: ["info", "danger", "warning", "success"].sample,
     url: Faker::Internet.url(host: 'test.lesli.cloud'),
-    role_receiver_names: (Role.all.sample).name
+    role_receiver_names: [(Role.all.sample).name],
+    media: Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg"),
+    payload: {},
+    notification_type: ["message", "project-alert", "event-reminder", "overdue-task"].sample,
 }, {
     subject: Faker::Lorem.sentence(word_count: 3),
     body: Faker::Lorem.paragraph(sentence_count: 2),
     category: ["info", "danger", "warning", "success"].sample,
-    url: Faker::Internet.url(host: 'test.lesli.cloud')
+    url: Faker::Internet.url(host: 'test.lesli.cloud'),
+    media: Faker::Avatar.image(slug: "my-own-slug", size: "50x50", format: "jpg"),
+    payload: {},
+    notification_type: ["message", "project-alert", "event-reminder", "due-task"].sample,
 }].each do |notification|
 
     RSpec.describe 'GET:/bell/notifications', type: :request do
