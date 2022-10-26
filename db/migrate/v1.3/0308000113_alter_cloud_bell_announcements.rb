9364a8c4327a097bf2dfd9274b5ae4a6b9a0a968
Luis Donis <ldonis.emc2@gmail.com>
2022-10-26 07:27:29 -0600

    fix: migration version #minor

--- db/migrate/v1.3/0308000113_alter_cloud_bell_announcements.rb
+++ db/migrate/v1.3/0308000113_alter_cloud_bell_announcements.rb
@@ -1,4 +1,4 @@
-class AlterCloudBellAnnouncements < ActiveRecord::Migration[6.0]
+class AlterCloudBellAnnouncements < ActiveRecord::Migration[7.0]
     def change
         change_column(:cloud_bell_announcements, :message, :text)
     end
