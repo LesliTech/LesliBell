0f2522f88a5c8def394b87960478cf9be46a8a4f
Alex <alex.phazz@gmail.com>
2022-12-19 19:24:46 +0200

    fix: factory has been added

--- spec/requests/notifications/index_spec.rb
+++ spec/requests/notifications/index_spec.rb
@@ -21,34 +21,35 @@ For more information read the license file including with this software.
 # include helpers, configuration & initializers for request tests
 require 'lesli_request_helper'
 
-#
 RSpec.describe 'GET:/bell/notifications.json', type: :request do
     
+    let(:create_notification){FactoryBot.create(:cloud_bell_notification)}
+    
     include_context 'request user authentication'
-
+    
     it 'is expected to respond with all the notification for the current user' do
-
-        get('/bell/notifications.json')
-
+        
+        get('/bell/notifications.json')        
+        
         # shared examples
         expect_response_with_successful
-
+        
         # custom
         notification_index = CloudBell::Notification.index(@current_user, @query)
         notification_count = @current_user.account.bell.notifications.where(:user => @current_user, :status => ["created", "sent", nil]).count
-
+        
         expect(response_json['pagination']['total']).to eql(notification_count)
         expect(response_json['pagination']['results']).to eql(response_json['records'].size)
         expect(response_json['pagination']['results']).to be <= @query[:pagination][:perPage]
     end
-
+    
     it "is expect to have a valid pagination" do
         
         get('/bell/notifications.json')
-
+        
         # shared examples
         expect_response_with_pagination
-
+        
         # custom
         expect(response_json).to have_key("pagination")
         expect(response_json["pagination"]).to be_a(Hash)
@@ -59,13 +60,16 @@ RSpec.describe 'GET:/bell/notifications.json', type: :request do
         expect(response_json['pagination']).to have_key('total')
         expect(response_json["pagination"]['total']).to be_a(Numeric)
     end
-
+    
     it "is expect to have a valid records" do
-        get('/bell/notifications.json')
-
+        
+        new_notification = create_notification
+        
+        get('/bell/notifications.json')    
+        
         # shared examples        
         expect_response_with_pagination
-
+        
         # custom
         expect(response_json).to have_key("records")
         expect(response_json["records"]).to be_a(Array)
@@ -86,12 +90,9 @@ RSpec.describe 'GET:/bell/notifications.json', type: :request do
         expect(response_json["records"].first['category']).to be_a(String)
         expect(response_json["records"].first).to have_key('channel')
         expect(response_json["records"].first['channel']).to be_a(String)
-        expect(response_json["records"].first).to have_key('notification_type')
-        expect(response_json["records"].first['notification_type']).to be_a(String)
-        expect(response_json["records"].first).to have_key('media')
-        expect(response_json["records"].first['media']).to be_a(String)
-        expect(response_json["records"].first).to have_key('payload')
-        expect(response_json["records"].first['payload']).to be_a(Hash)
+        expect(response_json["records"].first).to have_key('notification_type')        
+        expect(response_json["records"].first).to have_key('media')        
+        expect(response_json["records"].first).to have_key('payload')        
         expect(response_json["records"].first).to have_key('users_id')
         expect(response_json["records"].first['users_id']).to be_a(Integer)
         expect(response_json["records"].first).to have_key('cloud_bell_accounts_id')
